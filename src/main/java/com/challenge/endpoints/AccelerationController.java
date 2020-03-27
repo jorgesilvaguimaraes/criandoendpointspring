6a8d13dbc06090ca2465a5b0050516092e66ccfa
Jorge da Silva Guimarães <jorgeguimaraes@cinbesa.com.br>
2020-03-27 10:32:10 -0300

    Criando EndPoint com Spring Codenation Java Online

---
+++ src/main/java/com/challenge/endpoints/AccelerationController.java
@@ -0,0 +1,33 @@
+package com.challenge.endpoints;
+
+import com.challenge.entity.Acceleration;
+import com.challenge.service.impl.AccelerationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.websocket.server.PathParam;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/acceleration")
+public class AccelerationController {
+
+    @Autowired
+    private AccelerationService accelerationService;
+
+    @GetMapping("/{id}")
+    public Optional<Acceleration> findById(@PathVariable("id") Long id)
+    {
+        return accelerationService.findById(id);
+    }
+
+    @GetMapping
+    public List<Acceleration> findByCompanyId(@PathParam("companyId") Long companyId) {
+        return accelerationService.findByCompanyId(companyId);
+    }
+
+}
